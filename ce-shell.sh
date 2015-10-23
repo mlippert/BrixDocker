d45a875d383f84992e8bcaa867389f2b2a0a5a65
Mike Lippert <mlippert255@gmail.com>
2015-10-23 09:05:11 -0400

    Added ce-shell.sh to allow easy switching of nodejs version for testing the correctness engine

---
+++ ce-shell.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#NODE_VERSION=0.10
+NODE_VERSION=4.1.2
+
+# Default paths to the brix working directories
+CORRECTNESS_ENGINE_PATH=~/Projects/pearson/correctness_engine/
+
+# include .brixdevrc if it exists to redefine the variables w/ values of locations of
+# working directories for the brixclient, brixserver and correctness_engine
+if [ -f "$HOME/.brixdevrc" ]
+then
+    . "$HOME/.brixdevrc"
+fi
+
+
+# It attaches to my correctness_engine repo working directory so whatever I have there is what will run
+docker run --rm -it -v ${CORRECTNESS_ENGINE_PATH}:/app -w /app --name ce-shell node:${NODE_VERSION} bash
