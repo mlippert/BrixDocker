a10dabf53b1614b155f0a6e707e172bbdaf2ed2e
Mike Lippert <mlippert255@gmail.com>
2015-11-16 11:27:23 -0500

    Change ips-shell script to use node v0.12 instead of v4.1.2 which causes PI issues
    also publish ports 8088 & 8080 to make testing against this container w/ the testpage easier.

--- ips-shell.sh
+++ ips-shell.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-#NODE_VERSION=0.10
-NODE_VERSION=4.1.2
+# There are some issues w/ PI using the latest version (4+) version of nodejs (integration tests fail) -mjl 11/16/2015
+NODE_VERSION=0.12
+#NODE_VERSION=4.1.2
 
 # Default paths to the brix working directories
 BRIXSERVER_PATH=~/Projects/pearson/brixserver/
@@ -15,5 +16,7 @@ fi
 
 
 # It attaches to my brixserver repo working directory so whatever I have there is what will run
+# publish the 8088 & 8080 ports, so they can be accessed from the host (the former is the port usually
+# set by local ips config, while the latter is used by the deployed ips servers)
 docker start redis-server brixCE
-docker run --rm -it --link redis-server --link brixCE -v ${BRIXSERVER_PATH}:/app -w /app --name ips-shell node:${NODE_VERSION} bash
+docker run --rm -it --link redis-server --link brixCE -p 8088:8088 -p 8080:8080 -v ${BRIXSERVER_PATH}:/app -w /app --name ips-shell node:${NODE_VERSION} bash
