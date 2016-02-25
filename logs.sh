e0c3073e8224c869af10826386cb183f3b2cc338
Mike Lippert <mlippert255@gmail.com>
2016-02-25 09:29:49 -0500

    update logs.sh script to use the 1st argument as the container name, defaulting to ips if not given

--- logs.sh
+++ logs.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-# This script needs enhancements. pass the container name on the commandline for one.
-# Also note that it expects bunyan to be installed in the folder you're running the script
+# Note that this script expects bunyan to be installed in the folder you're running the script
 # from (npm bunyan).
 
-CONTAINER_NAME=ips
+# Container name is 1st argument, defaults to ips if not given
+CONTAINER_NAME=${1:-ips}
 
 # To pipe the stderr logs from the container to bunyan, redirect stderr to stdout, stdout to the bit bucket, and then pipe to bunyan
 docker logs -f $CONTAINER_NAME 2>&1 > /dev/null | node_modules/bunyan/bin/bunyan
